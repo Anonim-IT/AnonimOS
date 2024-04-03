4bfdcdc2be5594139fa43daeed14db7928afec5a
Anonim <79274085+Anonim-IT@users.noreply.github.com>
2024-04-03 11:33:41 +0200

    startmanj.sh

---
+++ startmanj.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###setfont cyr-sun16
+setfont ter-v32b
+
+# #-----------  Переход по выбору  ------------------------
+
+# case $OPTION in
+#                 "1")
+                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1.sh)"
+#              break
+#              ;;
+#                 "2")
+#                 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1-en.sh)"
+             
+#              break
+#              ;;
+#                 255)
+#      	        echo " ESC.";;
+# esac
